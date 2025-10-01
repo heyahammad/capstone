e63bc0e8e0aa98e64d2599cbfc8991ecd4d4d8c5
MD. FAISAL AHAMMAD <fahad.ahmed2456@gmail.com>
2025-10-01 21:29:31 +0600

    mod

--- blockchain.py
+++ blockchain.py
@@ -6,23 +6,22 @@ from flask import Flask, jsonify
 #block er kaj kam ekhane shuru hoilo
 
 class Blockchain:
+
     def __init__(self):
         self.chain = []
-        self.cerate_block(proof = 1, previous_hash = '0')
+        self.create_block(proof = 1, previous_hash = '0')
 
     def create_block(self, proof, previous_hash):
-        block = {
-            'index' : len(self.chain)+1,
-            'timestamp' : str(datetime.datetime.now()),
-            'proof' : proof,
-            'previous_hash' : previous_hash,
-            }
+        block = {'index': len(self.chain) + 1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'previous_hash': previous_hash}
         self.chain.append(block)
         return block
-    
+
     def get_previous_block(self):
         return self.chain[-1]
-    
+
     def proof_of_work(self, previous_proof):
         new_proof = 1
         check_proof = False
@@ -31,9 +30,9 @@ class Blockchain:
             if hash_operation[:4] == '0000':
                 check_proof = True
             else:
-                new_proof =+ 1
-            return new_proof
-        
+                new_proof += 1
+        return new_proof
+    
     def hash(self, block):
         encoded_block = json.dumps(block, sort_keys = True).encode()
         return hashlib.sha256(encoded_block).hexdigest()
@@ -51,9 +50,8 @@ class Blockchain:
             if hash_operation[:4] != '0000':
                 return False
             previous_block = block
-            block_index =+ 1
+            block_index += 1
         return True
-    
 
 #web er kaj kam ekhane shuru hoilo
 
