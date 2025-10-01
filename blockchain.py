934f453ade70393a8fb5dc0c6f24560fec9205c2
heyahammad <fahad.ahmed2456@gmail.com>
2025-10-01 18:15:10 +0600

    na

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,17 @@
-print('bhag bainchod')
+import datetime
+import hashlib
+import json
+from flask import Flask, jsonify
+
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.cerate_block(proof = 1, previous_hash = '0')
+
+    def create_block(self, proof, previous_hash):
+        block = {
+            'index' : len(self.chain)+1,
+            'timestamp' : str(datetime.datetime.now()),
+            'proof' : proof,
+            'previous_hash' : previous_hash,
+            }
