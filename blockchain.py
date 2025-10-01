32464e3b39b60b1e2aa63ef4cb0dd26b633dd7a1
heyahammad <fahad.ahmed2456@gmail.com>
2025-10-01 19:22:17 +0600

    na

--- blockchain.py
+++ blockchain.py
@@ -15,3 +15,8 @@ class Blockchain:
             'proof' : proof,
             'previous_hash' : previous_hash,
             }
+        self.chain.append(block)
+        return block
+    
+    def get_previous_block(self):
+        return self.chain[-1]
