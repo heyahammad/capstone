2fb89e9facaf88094be4fc95bacdc5524f37d739
MD. FAISAL AHAMMAD <fahad.ahmed2456@gmail.com>
2025-10-16 13:57:51 +0600

    hash

--- block.py
+++ block.py
@@ -10,3 +10,14 @@ class Block:
         self.previous_hash = previous_hash
         self.data = data
         self.hash = self.calculate_hash()
+    
+    def calculate_hash(self):
+        block_string = json.dumps({
+            'index': self.index,
+            'timestamp': self.timestamp,
+            'proof': self.proof,
+            'previous_hash': self.previous_hash,
+            'data': self.data
+        }, sort_keys=True).encode()
+        
+        return hashlib.sha256(block_string).hexdigest()
